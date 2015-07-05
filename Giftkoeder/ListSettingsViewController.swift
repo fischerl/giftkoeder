301c461cf771cbfe31cd0db54c3ae6dca3355990
fischerl <lisafischer90me.com>
2015-07-05 11:29:39 +0200

    scrollViews

---
+++ Giftkoeder/ListSettingsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SettingsViewController.swift
+//  Giftkoeder
+//
+//  Created by Lisa Fischer on 15.06.15.
+//  Copyright (c) 2015 TeamGK. All rights reserved.
+//
+
+import UIKit
+
+
+class ListSettingsViewController: UIViewController {
+    
+
+    
+    var mapTypeString:String!
+    var radius:Float = 0.0
+    var showAllWarningsBool:Bool = false
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        
+        
+        
+        
+        
+        
+    }
+    
+    @IBAction func showAllSwitchChanged(sender:UISwitch)
+    {
+
+        
+    }
+    
+    @IBAction func radiusChanged(sender:UISlider) {
+
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
